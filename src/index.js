73a354c6c7010fb9ed98a49ade3c05140b02494d
thejsn <thejsn@users.noreply.github.com>
2020-07-27 11:19:22 +0200

    Ignore source files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Promise from 'bluebird';
-import App from './components/App';
-
-window.Promise = Promise;
-
-render(<App />, document.getElementById('app'));
